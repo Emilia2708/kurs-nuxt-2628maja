d5a6f8a093d0af6fe2d1bfabd9e4ddfd93aa7a93
Emilia Majewska <emilia.majewska27@wp.pl>
2025-05-26 11:34:06 +0200

    inicjalizacja projektu

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <AppHeader />
+    <pierwszy />
+    <test />
+    <natalia />
+    <drugi/>
+    <trzeci />
+    <AppFooter></AppFooter>
+  </div>
+
+</template>
---
+++ components/drugi.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button class="inc" @click="increments">Drugi post {{ count }}</button>
+    <button class="inc" @click="showHideUsers">Lista postów </button>
+
+    <ul v-if="showuser">
+      <li>1 post</li>
+      <li>2 post</li>
+      <li>3 post</li>
+      <li>4 post</li>
+    </ul>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+const count = ref(0)
+const showuser = ref(false)
+
+const increments = () => {
+  count.value++
+}
+
+const showHideUsers = () => {
+  showuser.value = !showuser.value
+}
+
+</script>
+
+<style scoped>
+.inc {
+  padding: 10px 10px 10px;
+  background-color: aqua;
+  border: unset;
+  cursor: pointer;
+  font-size: 15px;
+}
+</style>
