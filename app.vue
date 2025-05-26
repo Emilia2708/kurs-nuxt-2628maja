a84d7f92e306f85972a777b8673074273d117365
Emilia Majewska <emilia.majewska27@wp.pl>
2025-05-26 13:07:30 +0200

    Dodanie Modal

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
 <template>
-  <FormLogin />
+  <button
+    @click="emitModalShow"
+      class="pc-4 py-2 bg-blue-500 text-white hover:bg-blue-600"
+  >Otwórz modal</button>
+
+  <Modal @modalValue="emitModalShow" :modalValue="showModal"/>
 </template>
+<script setup>
+const showModal = ref(false)
+
+const emitModalShow = () => {
+  showModal.value = !showModal.value
+}
+</script>
