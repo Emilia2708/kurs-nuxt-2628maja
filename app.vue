0e57a9c0a0705d173eaa1d58cc710eb7aa7dce2b
Emilia Majewska <emilia.majewska27@wp.pl>
2025-05-26 13:11:22 +0200

    dodanie stringa do komponenetu rodzica i przekazanie do komponentu dziecka

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
       class="pc-4 py-2 bg-blue-500 text-white hover:bg-blue-600"
   >Otwórz modal</button>
 
-  <Modal @modalValue="emitModalShow" :modalValue="showModal"/>
+  <Modal @modalValue="emitModalShow" :modalValue="showModal" :nameuser="'Szymon Guzik'"/>
 </template>
 <script setup>
 const showModal = ref(false)
--- components/Modal.vue
+++ components/Modal.vue
@@ -4,7 +4,7 @@
       <button @click="close" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700">X</button>
       <div class="p-6">
         <h2 class="text-xl font-bold mb-4">To jest modal</h2>
-        <p class="text-gray-700">Tresć modalnego okna</p>
+        <p class="text-gray-700">{{ nameuser }}</p>
       </div>
     </div>
   </div>
@@ -15,6 +15,10 @@ const props = defineProps({
   modalValue: {
     type: Boolean,
     required: true
+  },
+
+  nameuser: {
+    type: String
   }
 })
 
